4ab1296d7bbeb3acfb0d94d853ad375f7b9d85e0
sagarkmr11042005-boop <sagarkmr11042005@gmail.com>
2025-12-23 14:39:40 +0530

    Add files via upload

---
+++ B.Tech CSE/practical/javascript/loops.js
@@ -0,0 +1,50 @@
+// let i;
+// for(i=0;i<=20; i++){
+
+//     console.log(i);
+// }
+//.Q Print numbers from 1 to 50 using a for loop.
+
+//Print the table of 7 using a for loop.
+
+//Print all even numbers from 2 to 20.
+
+//Print the sum of first 10 natural numbers.
+
+//Print all numbers from 10 to 1 (reverse loop)
+
+// let i;
+// for(i=0;i<=50; i++){
+
+//     console.log(i);
+// }
+
+// let i;
+// for(i=7;i<=70; i=i+7){
+
+//     console.log(i);
+// }
+
+// let i;
+// let n =40;
+// for(i=2;i<=2*n; i=i+2){
+
+//     console.log(i);
+// }
+
+// let i;
+// let sum=0;
+// for(i=1;i<=10; i=i+1){
+//     sum +=i
+
+//     console.log(sum);
+// }
+
+let i;
+
+for(i=10;i>=1; i=i--){
+    
+
+    console.log(i);
+}
+
