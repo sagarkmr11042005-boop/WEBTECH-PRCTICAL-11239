a55f599468f0bcc7ee4454e976175c5ecc71e63f
Sagar Kumar <sagarkmr11042005@gmail.com>
2026-01-03 00:48:26 +0530

    Add files via upload

---
+++ B.Tech CSE/practical/practical_3_2.js
@@ -0,0 +1,15 @@
+{
+    var a = 10;      
+    let b = 20;      
+    const c = 30;    
+
+    console.log("Inside block:");
+    console.log("a =", a);
+    console.log("b =", b);
+    console.log("c =", c);
+}
+
+console.log("Outside block:");
+console.log("a =", a);   
+console.log("b =", b);   
+console.log("c =", c);   
